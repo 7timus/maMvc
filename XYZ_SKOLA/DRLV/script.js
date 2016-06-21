804e936cae33fcd774f5b6be5d71d2d6b8daecb9
7timus <7timus@gmail.com>
2016-06-21 21:03:34 +0300

    DRAUGIEM- HTML DONE

--- XYZ_SKOLA/DRLV/script.js
+++ XYZ_SKOLA/DRLV/script.js
@@ -2,28 +2,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var varianti = [];
     var i;
     var temp = {};
-    var old;
+     var old = "";
 
     varianti = document.getElementsByClassName('variants');
     console.log('IET');
     console.log(varianti.length);
 
 // SALIEKU VISIEM  variantiem funkciju
-   
-    
     for (i = 0; i != varianti.length; i++) {
-      console.log(typeof(varianti[i]));
-        varianti[i].addEventListener("click", function(){mark(this)});
+        varianti[i].addEventListener("click", function(){mark(this);});
     }
+    document.getElementById("nakamais").addEventListener("click", function(){nextQuestion();});
 
+
+// Iezime clickoto elementu
     function mark (obj) {
         temp.className = old;
         old = obj.className;
         temp = obj;
-            alert("temp added");
-             obj.innerHTML = "Clicked";
+        obj.className += " iezimets";
+    }
+// nakama jautajuma pogas funckija
+    function nextQuestion() {
+        if(old != "") {
+            console.log(old);
+            alert("Iezimets " + temp.innerHTML );
+        } else {
+            alert('Iezime kadu');
+        }
+
 
     }
-
 });
 
